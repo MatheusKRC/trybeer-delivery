494a5df25530ca52f0df535cd72a26df1ce9fa3b
Matheus leão <kyracraft0@gmail.com>
2023-07-27 13:42:15 -0300

    test vercel

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./Routes/UserRoutes');
-const productRoutes = require('./Routes/ProductsRoutes');
-const salesRoute = require('./Routes/SalesRoutes');
-const salesProductsRoute = require('./Routes/SalesProductsRoutes');
+// const productRoutes = require('./Routes/ProductsRoutes');
+// const salesRoute = require('./Routes/SalesRoutes');
+// const salesProductsRoute = require('./Routes/SalesProductsRoutes');
 
 const app = express();
 
@@ -11,9 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
 app.use(express.static('public'));
-app.use(productRoutes);
-app.use(salesRoute);
-app.use(salesProductsRoute);
+// app.use(productRoutes);
+// app.use(salesRoute);
+// app.use(salesProductsRoute);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
